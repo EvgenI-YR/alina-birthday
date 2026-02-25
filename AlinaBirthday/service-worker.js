9b5e9b3499e9bd6b9917b0d417c26532c2675660
Alina <alina@birthday.com>
2026-02-25 05:23:54 +0500

    Add Alina Birthday app

---
+++ AlinaBirthday/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'alina-birthday-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './alina.jpg',
+  './IMG_3074_final.mp4',
+  './IMG_1731_final.mp4'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
